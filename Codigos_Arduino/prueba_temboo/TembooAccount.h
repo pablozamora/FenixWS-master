6155c6cd4cdc68182790385102465f33e00c980b
pablozamora <pz112zamora@gail.com>
2015-05-25 20:37:30 -0600

    Prueba_temboo
    Prueba_temboo

---
+++ Codigos_Arduino/prueba_temboo/TembooAccount.h
@@ -0,0 +1,5 @@
+
+
+#define TEMBOO_ACCOUNT "pablozamora"  // your Temboo account name 
+#define TEMBOO_APP_KEY_NAME "pruebatemboo"  // your Temboo app key name
+#define TEMBOO_APP_KEY  "4261d4f75a3f462783984cd528723a32"  // your Temboo app key
